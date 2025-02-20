262bc743029ba1dbfedcc6ebfa583fb04527faba
stuffbymax <96355058+stuffbymax@users.noreply.github.com>
2025-02-20 15:53:09 +0000

    Create random.sh

---
+++ random.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wordList=("Tifa" "Aerith" "Damian" "Larry the cow" "PS3" "LLVM" "GCC" "CLang" "Portage" "Emerge" "Gotta catch em all" "Chicken boob" "Beetroot" "Booze" "Vape" "Linux" "Windows" "macOS" "Gentoo" "Arch" "Wake Up!")
+
+while true; do
+    sleep 1
+    randomWord=${wordList[$RANDOM % ${#wordList[@]}]}
+    
+    # Simulate typing the random word and pressing Enter
+    xdotool type "$randomWord"
+    xdotool key Return  # Press Enter
+done
